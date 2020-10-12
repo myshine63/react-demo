5a746b96362bb0870e07c6bb8320cefb78882858
hepan <pan@edmodo.com>
2020-10-12 18:00:45 +0800

    添加了一个使用render prop的案例

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ import {
   Route,
 } from "react-router-dom";
 import asyncLoad from "./utils/asyncLoad";
+import {Layout} from "antd";
 //--- page component ---//
 const Home = asyncLoad(() => import('./pages/home/Home'))
 const ErrorPage = asyncLoad(() => import('./pages/errorPage/ErrorPage'));
 const RefsPage = asyncLoad(() => import('./pages/refs/RefsPage'));
 const ContextPage = asyncLoad(() => import('./pages/context/ContextPage'));
+const RenderPropPage = asyncLoad(() => import('./pages/render-prop/RenderPropPage'));
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/' component={Home} exact/>
-        <Route path='/home' component={Home} exact={true}/>
-        <Route path='/err' component={ErrorPage} exact={true}/>
-        <Route path='/refs' component={RefsPage} exact={true}/>
-        <Route path='/context' component={ContextPage} exact={true}/>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path='/' component={Home} exact/>
+          <Route path='/home' component={Home} exact={true}/>
+          <Route path='/err' component={ErrorPage} exact={true}/>
+          <Route path='/refs' component={RefsPage} exact={true}/>
+          <Route path='/context' component={ContextPage} exact={true}/>
+          <Route path='/render-prop' component={RenderPropPage} exact={true}/>
+        </Switch>
+      </Layout>
     </Router>
   );
 }
---
+++ src/pages/render-prop/RenderPropPage.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import './RenderPropPage.scss'
+
+function UseColor({color}) {
+  return (<div className='use-color' style={{backgroundColor: color}}/>)
+}
+
+// 当useColor组件想要使用changeColor的内部状态时，直接使用render prop就可以了
+function RenderPropPage() {
+  return (
+    <div className='render-prop-page'>
+      <ChangeColor render={(color) => {
+        return <UseColor color={color}/>
+      }}/>
+    </div>
+  )
+}
+
+function ChangeColor({render}) {
+  const [color, setColor] = useState('green');
+  // 内部定义一个color状态
+  const clickBtn = (value) => {
+    setColor(value);
+  }
+  return (
+    <>
+      <button onClick={() => {
+        clickBtn('green')
+      }}>
+        green
+      </button>
+      <button onClick={() => {
+        clickBtn('red')
+      }}>
+        red
+      </button>
+      {/*这里可以接受各种想要使用ChangeColor 内部状态的组件*/}
+      {render(color)}
+    </>
+  )
+}
+
+export default RenderPropPage
